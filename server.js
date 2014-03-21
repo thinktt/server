34486343f2fb56049ee942a65d0369d4aaf5ec04
thinktt <github@thinktt.e4ward.com>
2014-03-20 19:36:30 -0500

    Change to correct Favicon location

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ function onListening() {
 app.use(express.logger('dev'));
 app.use(express.json()); 
 app.use(express.static(__dirname + '/public/'));
-app.use(express.favicon(__dirname + "/public/skull.ico")); 
+app.use(express.favicon(__dirname + "/ajax/skull.ico")); 
 app.use('/ajax', express.static(__dirname + '/ajax/'));
 app.post('/ajax', handlePost); 
 
