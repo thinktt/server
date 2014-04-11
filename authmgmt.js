0fe1a6dbd68140e9e76b680772d371677eef5ccf
thinktt <github@thinktt.e4ward.com>
2014-04-11 16:52:04 -0500

    Continues build out of authentication module
    This is a major work in progress

--- authmgmt.js
+++ authmgmt.js
@@ -2,67 +2,134 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
 var db; 
 
-mongoose.connect('mongodb://localhost/test');
+
+mongoose.connect('mongodb://localhost/users');
 db = mongoose.connection;
 
 var userSchema = mongoose.Schema({	
-	username: String,
+	username: {type: String, lowercase: true}, 
 	passHash: String,
-	sessionID: String,
-	sessionExp: Number
+	session: {
+		id: String,
+		experation: Number
+	}	
 });
-
 
 var User = mongoose.model('User', userSchema); 
 
-var user = new User(); 
-
-/*
-var user = new User({
-	username: '',
-	passHash: '',
-	sessionID: '',
-	sessionExp: 0
-});
-*/
-console.log(user); 
-console.log(user.username); 
 
 
-function addCredentials(username, password) {
-	var salt, hash, i;
+function addCredentials(username, password, callback) {
+	var salt, hash, i, user;
 
-	user.username = username;
+	//check if username exist
+	User.find({username: username}, function(error, userData){
+	    
+	    //if it doesn't exist then add it	    	    
+	    if(userData.length === 0) {
+			user = new User(); 
+			user.username = username;
+			
+			//generate a salt
+			bcrypt.genSalt(10, function(err, salt) {
+
+				//when salt is done hash the password
+		    	bcrypt.hash(password, salt, function(err, hash) {
+		        	
+		        	//whe hash is done store new credentials in db
+		        	user.passHash = hash; 
+		        	user.save(function (err) {
+						if(!err) {
+							console.log('credentials stored'); 
+							return callback('user added'); 
+						}
+					});
+
+		    	});
+			});
+		}
+		//otherwise user already exist
+		else {
+	       	return callback('user taken');
+	    }
+}
+	    
+
+/*function checkCredentials(username, password, callback) {
+	var user;
+
+	//get the user information
+	User.find({username: username}, function(error, userData) {
+		
+		//does the user exist
+		if(userData.length === 0) {
+			return callback('no such user');
+		}
+		else {
+			user = userData[0];  
+			return callback(user.username + ' exist!'); 
+		}
 	
-	//generate a password salt and hash
-	salt = bcrypt.genSaltSync(10);
-	user.passHash = bcrypt.hashSync(password, salt);
-
-	//make sure user.sessionID is clear
-	user.sessionID = ""; 
-
-	//generate a session ID by stringing 10 
-	//bcrypt salts together, only taking the
-	//random string part 
-	for(i=0; i<10; i++) {
-		user.sessionID = user.sessionID + bcrypt.genSaltSync(5).substr(7);
-	}
-
-
-	//generate a session experation
-	//an hour from the current time 
-	user.sessionExp = (Date.now() + 3600000);
+	});
 
 }
 
 
-addCredentials('legolas', 'xyzzy'); 
+
+checkCredentials('legolas', 'xyzzy', function(status) {
+	console.log(status); 
+});
+
+*/
+
+
+
+/*
+addCredentials('frodo', 'xyzzy', function(status){
+	console.log(status); 
+}); 
+
+	//check password
+	
+	//create sessionID
+	
+
+	//generate a session ID by stringing 10 
+	//bcrypt salts together, only taking the
+	//random string part 
+	//for(i=0; i<10; i++) {
+	//		user.sessionID = user.sessionID + bcrypt.genSaltSync(5).substr(7);
+	//}
+
+
+	//generate a session experation
+	//an hour from the current time 
+	//user.sessionExp = (Date.now() + 3600000);
+
+
+	//return sesionID 
+
+    //}
+    //	});
+
+User.find({username: 'toby'}, function(error, data){
+    console.log(data);
+ });
+
+
+function checkSessionID(sessionID) {
+
+}
+
+
+
 
 console.log(user); 
 console.log('username: ', user.username);
 console.log('\nhash: ', user.passHash); 
 console.log('\nsessionID:\n', user.sessionID);
-console.log('\nsessionExp:', user.sessionExp); 
+console.log('\nsessionExp:', user.sessionExp);
+
 
 
 
@@ -70,3 +137,4 @@ console.log('\nsessionExp:', user.sessionExp);
 //add credentials (username, password)
 //check username
 //check sessionID 
+*/
