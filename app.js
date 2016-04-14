d04a7b5133aef726ee7c4efbe7fcc9846b5720ee
thinktt <github@thinktt.e4ward.com>
2016-04-14 16:43:27 -0500

    add secure server

--- app.js
+++ app.js
@@ -1,47 +1,34 @@
 var express = require('express');
 var http = require('http');
+var https = require('https');
 var path = require('path');
-
+var fs = require('fs');
 var app = express();
+var port = process.env.PORT || 3000;
+var securePort = process.env.SECURE_PORT || 3443
+
+var sslOptions = {
+  key: fs.readFileSync('../cert/server.key'),
+  cert: fs.readFileSync('../cert/server.crt'),
+};
+
 app.use(express.logger('dev'));
 app.disable('x-powered-by');
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('port', process.env.PORT || 3000);
+
 
 app.use(function (req, res) { 
 	res.redirect("/");
 });
 
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(port, function(){
+  console.log('Express server listening on port ' + port);
+});
+
+https.createServer(sslOptions, app).listen(securePort, function(){
+  console.log('Secure Express server listening on port ' + securePort);
 });
 
 
-
-
-// if ('development' == app.get('env')) {
-//   app.use(express.errorHandler());
-// }
-//var user = require('./routes/user');
-//var routes = require('./routes');
-//app.get('/', routes.index);
-//app.get('/users', user.list);
-//app.use(app.router);
-// all environments
-//set port to eviroment pot or 3000
-//sets views folder to /views using dirname 
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
-//app.use(express.json());
-//app.use(express.favicon());
-//probaby turns on the logger
-//use json for sends??
-//urlencoded?
-//app.use(express.urlencoded());
-//to simulate delete and put??
-//app.use(express.methodOverride());
-//turns on routes (automatic if you define routes)
-//sets the static folder
-
-// development only
